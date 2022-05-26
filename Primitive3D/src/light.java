a3617d5aae27dd8e00dae007da5d9cdfd0b2578f
Yang <45927105+yang2158@users.noreply.github.com>
2022-05-26 12:52:38 -0700

    Delete Primitive3D directory
    no use anymore

--- Primitive3D/src/light.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Color;
-
-public class light {
-	Vector pos ;
-	Color tint= new Color( 255,255,255);
-	double intensity=0.5;
-	
-}
--- Primitive3D/src/test.java
+++
@@ -1,9 +0,0 @@
-
-public class test {
-	public static void main(String[] args) {
-		Shaders shader = new Shaders();
-		Vector a = new Vector( 0,-1,0);
-		Vector b = new Vector( 0,1,0);
-		
-	}
-}
